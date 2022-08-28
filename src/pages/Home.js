1be185f7835535eda1aa95af2565dd1d081af5d8
Stephen Magrowski <stephen.magrowski@gmail.com>
2022-08-28 15:55:46 -0400

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <p>
+          Home
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
